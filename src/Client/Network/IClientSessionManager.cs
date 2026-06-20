156d37e2fd3f5b6cfdebc4eef36c0b3b7539f1ea
Acu1000 <acu1000.developer@gmail.com>
2026-06-20 17:23:17 +0200

    add interfaces for session managers

---
+++ src/Client/Network/IClientSessionManager.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Protocol.Shared.Network.Packets;
+
+namespace Protocol.Client.Network;
+
+public interface IClientSessionManager
+{
+    public Task<bool> TryConnectToServer(IPEndPoint endPoint);
+    public void DisconnectFromServer();
+    public void SendToServer<T>(IPacket<T> packet) where T : IPacket<T>;
+
+    public void HandleConnectAcceptPacket(ReadOnlySpan<byte> packetData, EndPoint sourceEndPoint);
+    public void HandlePingPacket(ReadOnlySpan<byte> packetData, EndPoint sourceEndPoint);
+    public void HandlePongPacket(ReadOnlySpan<byte> packetData, EndPoint sourceEndPoint);
+    
+    // Verify if endpoint belongs to connected server
+    public bool IsServerEndpoint(IPEndPoint endpoint); 
+}
