77c9bed9d28408fd8d9d600bad7372cbcf9e61e9
Tomek2kk <162097355+Tomek2kk@users.noreply.github.com>
2026-06-21 18:36:44 +0200

    cl

--- src/Server/Network/Models/ClientInfo.cs
+++ src/Server/Network/Models/ClientInfo.cs
@@ -5,7 +5,8 @@ namespace Protocol.Server.Network.Models;
 
 public class ClientInfo
 {
-    public string Username;
+    public string Username { get; set; } = string.Empty;
+    public UInt16 ClientId { get; set; }
     public Int64 SessionToken;
-    public EndPoint EndPoint;
+    public EndPoint EndPoint { get; set; } = default!;
 }
