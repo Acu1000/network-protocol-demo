f35fd09d5bc8114167c644e58a76bbd300377c80
Acu1000 <acu1000.developer@gmail.com>
2026-06-29 02:24:32 +0200

    add first visuals

--- src/Shared/Scenes/Player/PlayerCharacter.cs
+++ src/Shared/Scenes/Player/PlayerCharacter.cs
@@ -6,34 +6,30 @@ namespace Protocol.Shared.Scenes.Player;
 
 public partial class PlayerCharacter : CharacterBody2D, IEntity
 {
-	public const float Speed = 6.0f;
+	[Export] private float _moveSpeed = 4.0f;
+	[Export] private float _turnSpeed = 3.0f;
+
+	[Export] private Node2D _turret;
 	
 	public bool Controlled { get; set; } = false;
 
 	public override void _PhysicsProcess(double delta)
 	{
-		if (Controlled) ProcessControls();
+		if (Controlled) ProcessControls(delta);
 	}
 
-	private void ProcessControls()
+	private void ProcessControls(double delta)
 	{
+		_turret.LookAt(GetGlobalMousePosition());
+		
 		Vector2 velocity = Velocity;
-        		
-        Vector2 direction = Input.GetVector(
-        	"left", 
-        	"right", 
-        	"up", 
-        	"down");
-        
-        if (direction != Vector2.Zero)
-        {
-        	velocity = direction * Speed;
-        }
-        else
-        {
-        	velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-        	velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
-        }
+
+		float inputMove = Input.GetAxis("up", "down");
+		float inputTurn = Input.GetAxis("left", "right");
+
+		Rotation += inputTurn * _turnSpeed * (float)delta;
+
+		velocity = Transform.Y * inputMove * _moveSpeed;
 
         Velocity = velocity;
         MoveAndSlide();
@@ -47,23 +43,27 @@ public partial class PlayerCharacter : CharacterBody2D, IEntity
 
 	public void WriteStateTo(Span<byte> buffer)
 	{
-		BitConverter.TryWriteBytes(buffer.Slice(0, 4), Position.X);
-		BitConverter.TryWriteBytes(buffer.Slice(4, 4), Position.Y);
+		BitConverter.TryWriteBytes(buffer.Slice(0, 4), GlobalPosition.X);
+		BitConverter.TryWriteBytes(buffer.Slice(4, 4), GlobalPosition.Y);
+		BitConverter.TryWriteBytes(buffer.Slice(8, 4), GlobalRotation);
+		BitConverter.TryWriteBytes(buffer.Slice(12, 4), _turret.GlobalRotation);
 	}
 
 	public byte[] GetState()
 	{
-		byte[] buffer = new byte[8];
+		byte[] buffer = new byte[16];
 		WriteStateTo(buffer);
 		return buffer;
 	}
 
 	public void UpdateState(ReadOnlySpan<byte> state)
 	{
-		Position = new Vector2(
+		GlobalPosition = new Vector2(
 			BitConverter.ToSingle(state.Slice(0, 4)),
 			BitConverter.ToSingle(state.Slice(4, 4))
 			);
+		GlobalRotation = BitConverter.ToSingle(state.Slice(8, 4));
+		_turret.GlobalRotation = BitConverter.ToSingle(state.Slice(12, 4));
 	}
 
 	public void Delete()
